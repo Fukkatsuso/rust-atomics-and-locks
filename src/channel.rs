6cacbbeec76aa277b1b0e28771fc6abe4da6c8cc
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2025-04-06 16:25:43 +0900

    channel: unsafe なワンショットチャネル

--- src/channel.rs
+++ src/channel.rs
@@ -1,34 +1,36 @@
-use std::{
-    collections::VecDeque,
-    sync::{Condvar, Mutex},
-};
+use std::{cell::UnsafeCell, mem::MaybeUninit, sync::atomic::AtomicBool};
+
+use std::sync::atomic::Ordering::*;
 
 pub struct Channel<T> {
-    queue: Mutex<VecDeque<T>>,
-    item_ready: Condvar,
+    message: UnsafeCell<MaybeUninit<T>>,
+    ready: AtomicBool,
 }
 
+// 少なくとも T が Send であれば、このチャネルをスレッド間で共有しても安全だ、ということをコンパイラに示す
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
 impl<T> Channel<T> {
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self {
-            queue: Mutex::new(VecDeque::new()),
-            item_ready: Condvar::new(),
+            message: UnsafeCell::new(MaybeUninit::uninit()),
+            ready: AtomicBool::new(false),
         }
     }
 
-    pub fn send(&self, message: T) {
-        self.queue.lock().unwrap().push_back(message);
-        self.item_ready.notify_one();
+    /// 安全性:1 度しか呼んではいけない !
+    pub unsafe fn send(&self, message: T) {
+        (*self.message.get()).write(message);
+        self.ready.store(true, Release);
     }
 
-    pub fn receive(&self) -> T {
-        let mut b = self.queue.lock().unwrap();
-        loop {
-            if let Some(message) = b.pop_front() {
-                return message;
-            }
-            // wait: 待機中は Mutex をアンロックし、リターンする前に再度ロックする
-            b = self.item_ready.wait(b).unwrap();
-        }
+    pub fn is_ready(&self) -> bool {
+        self.ready.load(Acquire)
+    }
+
+    /// 安全性:このメソッドは 1 度だけ呼ぶこと。
+    /// また、is_ready() が true を返した場合にだけ呼ぶこと。
+    pub unsafe fn receive(&self) -> T {
+        (*self.message.get()).assume_init_read()
     }
 }
