532143a091d393711d99dde8e57c832d12a51aa0
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2025-04-12 12:43:43 +0900

    arc: weak ポインタ

--- src/arc.rs
+++ src/arc.rs
@@ -1,41 +1,89 @@
+use std::cell::UnsafeCell;
 use std::sync::atomic::{fence, Ordering::*};
 use std::usize;
 use std::{ops::Deref, ptr::NonNull, sync::atomic::AtomicUsize};
 
 struct ArcData<T> {
-    ref_count: AtomicUsize,
-    data: T,
+    /// `Arc` の数
+    data_ref_count: AtomicUsize,
+    /// 'Arc` と `Weak` の数を足したもの
+    alloc_ref_count: AtomicUsize,
+    /// データ本体。weak ポインタしか残っていなければ `None` になる
+    data: UnsafeCell<Option<T>>,
 }
 
 pub struct Arc<T> {
+    weak: Weak<T>,
+}
+
+pub struct Weak<T> {
     ptr: NonNull<ArcData<T>>,
 }
 
-unsafe impl<T: Send + Sync> Send for Arc<T> {}
-unsafe impl<T: Send + Sync> Sync for Arc<T> {}
+unsafe impl<T: Send + Sync> Send for Weak<T> {}
+unsafe impl<T: Send + Sync> Sync for Weak<T> {}
 
 impl<T> Arc<T> {
     pub fn new(data: T) -> Arc<T> {
         Arc {
-            ptr: NonNull::from(Box::leak(Box::new(ArcData {
-                ref_count: AtomicUsize::new(1),
-                data,
-            }))),
+            weak: Weak {
+                ptr: NonNull::from(Box::leak(Box::new(ArcData {
+                    alloc_ref_count: AtomicUsize::new(1),
+                    data_ref_count: AtomicUsize::new(1),
+                    data: UnsafeCell::new(Some(data)),
+                }))),
+            },
         }
     }
 
     fn data(&self) -> &ArcData<T> {
-        unsafe { self.ptr.as_ref() }
+        unsafe { self.weak.ptr.as_ref() }
     }
 
     pub fn get_mut(arc: &mut Self) -> Option<&mut T> {
-        if arc.data().ref_count.load(Relaxed) == 1 {
+        if arc.weak.data().alloc_ref_count.load(Relaxed) == 1 {
             fence(Acquire);
-            // 安全性:Arc は 1 つしかないので、他の何もデータにアクセスできない。
+            // 安全性:Arc は 1 つしかないし weak ポインタはないので、他の何もデータにアクセスできない。
             // その Arc に対してこのスレッドが排他アクセス権限を持っている。
-            unsafe { Some(&mut arc.ptr.as_mut().data) }
+            let arcdata = unsafe { arc.weak.ptr.as_mut() };
+            let option = arcdata.data.get_mut();
+            // Arc があるのでデータがまだ利用できることがわかっている。
+            // したがってここでパニックすることはない
+            let data = option.as_mut().unwrap();
+            Some(data)
         } else {
             None
+        }
+    }
+
+    pub fn downgrade(arc: &Self) -> Weak<T> {
+        arc.weak.clone()
+    }
+}
+
+impl<T> Weak<T> {
+    fn data(&self) -> &ArcData<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+
+    pub fn upgrade(&self) -> Option<Arc<T>> {
+        let mut n = self.data().data_ref_count.load(Relaxed);
+        loop {
+            if n == 0 {
+                return None;
+            }
+            assert!(n < usize::MAX);
+            if let Err(e) =
+                self.data()
+                    .data_ref_count
+                    .compare_exchange_weak(n, n + 1, Relaxed, Relaxed)
+            {
+                // 現在の値が n でなかった（カウンタが更新されていた）ので比較交換に失敗
+                // 次のループでリトライする
+                n = e;
+                continue;
+            }
+            return Some(Arc { weak: self.clone() });
         }
     }
 }
@@ -44,22 +92,49 @@ impl<T> Deref for Arc<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        &self.data().data
+        let ptr = self.weak.data().data.get();
+        // 安全性:データに対する Arc があるので、
+        // データは存在するし、共有されているかもしれない。
+        unsafe { (*ptr).as_ref().unwrap() }
     }
 }
 
 impl<T> Clone for Arc<T> {
     fn clone(&self) -> Self {
-        if self.data().ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+        let weak = self.weak.clone();
+        if weak.data().data_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
             std::process::abort();
         }
-        Arc { ptr: self.ptr }
+        Arc { weak }
+    }
+}
+
+impl<T> Clone for Weak<T> {
+    fn clone(&self) -> Self {
+        if self.data().alloc_ref_count.fetch_add(1, Relaxed) > usize::MAX / 2 {
+            std::process::abort();
+        }
+        Weak { ptr: self.ptr }
     }
 }
 
 impl<T> Drop for Arc<T> {
     fn drop(&mut self) {
-        if self.data().ref_count.fetch_sub(1, Release) == 1 {
+        if self.weak.data().data_ref_count.fetch_sub(1, Release) == 1 {
+            fence(Acquire);
+            let ptr = self.weak.data().data.get();
+            // 安全性:データへの参照カウントはゼロなので、
+            // 他の場所からアクセスすることはない。
+            unsafe {
+                (*ptr) = None;
+            }
+        }
+    }
+}
+
+impl<T> Drop for Weak<T> {
+    fn drop(&mut self) {
+        if self.data().alloc_ref_count.fetch_sub(1, Release) == 1 {
             fence(Acquire);
             unsafe {
                 drop(Box::from_raw(self.ptr.as_ptr()));
@@ -80,29 +155,26 @@ fn test() {
         }
     }
 
-    // 文字列と DetectDrop を保持するオブジェクトを共有する
-    // Arc を 2 つ作る。DetectDrop でいつドロップされたかわかる
+    // この時点では weak ポインタはアップグレード可能
     let x = Arc::new(("hello", DetectDrop));
-    let y = x.clone();
+    let y = Arc::downgrade(&x);
+    let z = Arc::downgrade(&x);
 
-    // x をもう 1 つのスレッドに送り、そこで使う
     let t = std::thread::spawn(move || {
-        assert_eq!(x.0, "hello");
+        // この時点では weak ポインタはアップグレード可能
+        let y = y.upgrade().unwrap();
+        assert_eq!(y.0, "hello");
     });
-
-    // 同時に y はこちらで使えるはず
-    assert_eq!(y.0, "hello");
-
-    // スレッドが終了するのを待機
+    assert_eq!(x.0, "hello");
     t.join().unwrap();
 
-    // Arc xはここまででドロップされているはず
-    // まだ y があるので、オブジェクトはまだドロップされない
+    // データはまだドロップされていないはずなので、weakポインタはアップグレード可能
     assert_eq!(NUM_DROPS.load(Relaxed), 0);
+    assert!(z.upgrade().is_some());
 
-    // 残った `Arc` をドロップ
-    drop(y);
+    drop(x);
 
-    // `y`もドロップしたので、オブジェクトもドロップされたはず
+    // データはドロップされているはずなので、weakポインタはアップブレード不可能
     assert_eq!(NUM_DROPS.load(Relaxed), 1);
+    assert!(z.upgrade().is_none());
 }
