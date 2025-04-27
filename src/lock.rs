78e48abc33ded09082751848f98135ce81380a3a
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2025-04-27 15:08:24 +0900

    9.3 initial RwLock

--- src/lock.rs
+++ src/lock.rs
@@ -1,11 +1,11 @@
 use std::sync::atomic::{AtomicUsize, Ordering::*};
-use std::thread;
 use std::time::Duration;
 use std::{
     cell::UnsafeCell,
     ops::{Deref, DerefMut},
     sync::atomic::AtomicU32,
 };
+use std::{thread, u32};
 
 use atomic_wait::{wait, wake_all, wake_one};
 
@@ -160,4 +160,92 @@ fn test_condvar() {
     // メインスレッドが(ビジーループではなく)実際にウェイトしたことをチェック。
     // ただし、何度か誤って起こされることは許容する。
     assert!(wakeups < 10);
+}
+
+pub struct RwLock<T> {
+    /// リーダの数。ライトロックされている場合には u32::MAX
+    state: AtomicU32,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for RwLock<T> where T: Send + Sync {}
+
+impl<T> RwLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(0), // unlocked
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn read(&self) -> ReadGuard<T> {
+        let mut s = self.state.load(Relaxed);
+        loop {
+            if s < u32::MAX {
+                assert!(s != u32::MAX - 1, "too many readers");
+                match self.state.compare_exchange_weak(s, s + 1, Acquire, Relaxed) {
+                    Ok(_) => return ReadGuard { rwlock: self },
+                    Err(e) => s = e,
+                }
+            }
+            if s == u32::MAX {
+                wait(&self.state, u32::MAX);
+                s = self.state.load(Relaxed);
+            }
+        }
+    }
+
+    pub fn write(&self) -> WriteGuard<T> {
+        while let Err(s) = self.state.compare_exchange(0, u32::MAX, Acquire, Relaxed) {
+            // ロックされていたら待機する
+            wait(&self.state, s);
+        }
+        WriteGuard { rwlock: self }
+    }
+}
+
+pub struct ReadGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+pub struct WriteGuard<'a, T> {
+    rwlock: &'a RwLock<T>,
+}
+
+impl<T> Deref for WriteGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> DerefMut for WriteGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        unsafe { &mut *self.rwlock.value.get() }
+    }
+}
+
+impl<T> Deref for ReadGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        unsafe { &*self.rwlock.value.get() }
+    }
+}
+
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        if self.rwlock.state.fetch_sub(1, Release) == 1 {
+            // state が 0 になるので、待機中のライタがいれば、それを起こす
+            wake_one(&self.rwlock.state);
+        }
+    }
+}
+
+impl<T> Drop for WriteGuard<'_, T> {
+    fn drop(&mut self) {
+        self.rwlock.state.store(0, Release);
+        // 待機しているライタを 1 つ、もしくはリーダをすべて起こす
+        // 待機しているのがリーダなのかライタなのかわからないし、どちらかだけを起こすこともできないため、すべてのスレッドを起こす
+        wake_all(&self.rwlock.state);
+    }
 }
