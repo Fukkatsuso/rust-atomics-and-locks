82684890e764a3bda8b1d54e09e6b71e23d87f74
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2025-04-27 11:33:50 +0900

    9.1.1 システムコールを避ける

--- src/lock.rs
+++ src/lock.rs
@@ -9,7 +9,8 @@ use atomic_wait::{wait, wake_all, wake_one};
 
 pub struct Mutex<T> {
     /// 0: unlocked
-    /// 1: locked
+    /// 1: locked, 他の待機スレッドはない
+    /// 2: locked, 他に待機スレッドがある
     state: AtomicU32,
     value: UnsafeCell<T>,
 }
@@ -25,7 +26,7 @@ unsafe impl<T> Sync for MutexGuard<'_, T> where T: Sync {}
 impl<T> Deref for MutexGuard<'_, T> {
     type Target = T;
     fn deref(&self) -> &T {
-        unsafe { &*self.mutex.value.get() }
+        unsafe { &*self.mutex.value.get() } // &*: 生ポインタを参照に変換している
     }
 }
 
@@ -44,10 +45,12 @@ impl<T> Mutex<T> {
     }
 
     pub fn lock(&self) -> MutexGuard<T> {
-        // state を 1 (locked) にセットする
-        while self.state.swap(1, Acquire) == 1 {
-            // すでにロックされていたら、state が 1 でなくなる (unlocked) まで待機する
-            wait(&self.state, 1);
+        if self.state.compare_exchange(0, 1, Acquire, Relaxed).is_err() {
+            // ここに到達したということは、Mutex はすでに state 1 or 2 でロックされている
+            // 起こされたときは state が 0 なので、自分で 2 に戻す
+            while self.state.swap(2, Acquire) != 0 {
+                wait(&self.state, 2);
+            }
         }
         MutexGuard { mutex: self }
     }
@@ -55,9 +58,9 @@ impl<T> Mutex<T> {
 
 impl<T> Drop for MutexGuard<'_, T> {
     fn drop(&mut self) {
-        // state を 0 (unlocked) に戻す
-        self.mutex.state.store(0, Release);
-        // 待機中のスレッドがあればその 1 つを起こす
-        wake_one(&self.mutex.state);
+        if self.mutex.state.swap(0, Release) == 2 {
+            // 待機中のスレッドがいる場合のみ、wake_one を呼び出す
+            wake_one(&self.mutex.state);
+        }
     }
 }
