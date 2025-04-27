c8c406123f2cd75c2c6767f381f53c0407495219
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2025-04-27 12:43:05 +0900

    9.2 initial Condvar

--- src/lock.rs
+++ src/lock.rs
@@ -1,4 +1,6 @@
 use std::sync::atomic::Ordering::*;
+use std::thread;
+use std::time::Duration;
 use std::{
     cell::UnsafeCell,
     ops::{Deref, DerefMut},
@@ -77,4 +79,68 @@ impl<T> Drop for MutexGuard<'_, T> {
             wake_one(&self.mutex.state);
         }
     }
+}
+
+pub struct Condvar {
+    counter: AtomicU32,
+}
+
+impl Condvar {
+    pub const fn new() -> Self {
+        Self {
+            counter: AtomicU32::new(0),
+        }
+    }
+
+    pub fn notify_one(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_one(&self.counter);
+    }
+
+    pub fn notify_all(&self) {
+        self.counter.fetch_add(1, Relaxed);
+        wake_all(&self.counter);
+    }
+
+    pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        let counter_value = self.counter.load(Relaxed);
+
+        // ガードをドロップすることで、Mutex をアンロックする
+        // ただし、後でロックするために mutex を覚えておく
+        let mutex = guard.mutex;
+        drop(guard);
+
+        // カウンタ値がアンロックする前から変更されていない場合にだけ待機する
+        wait(&self.counter, counter_value);
+
+        mutex.lock()
+    }
+}
+
+#[test]
+fn test_condvar() {
+    let mutex = Mutex::new(0);
+    let condvar = Condvar::new();
+
+    let mut wakeups = 0;
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            thread::sleep(Duration::from_secs(1));
+            *mutex.lock() = 123;
+            condvar.notify_one();
+        });
+
+        let mut m = mutex.lock();
+        while *m < 100 {
+            m = condvar.wait(m);
+            wakeups += 1;
+        }
+
+        assert_eq!(*m, 123);
+    });
+
+    // メインスレッドが(ビジーループではなく)実際にウェイトしたことをチェック。
+    // ただし、何度か誤って起こされることは許容する。
+    assert!(wakeups < 10);
 }
